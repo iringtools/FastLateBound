c9a992f561545ae9a62c1f03b296ec4882c80b0a
Rob DeCarlo <rdecarlo73@gmail.com>
2016-07-22 11:25:32 -0400

    Add project files.

---
+++ FastLateBoundGenerics/InterfaceWrapper.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace FastLateBoundGenerics
+{
+    class InterfaceWrapper
+    {
+        private static KnownT CreateNewObject<T, KnownT>() where T : KnownT, new()
+        {
+            return new T();
+        }
+
+        /// <summary>
+        /// Creates static creator delegate for specified type
+        /// </summary>
+        /// <typeparam name="T2">known parent type of specified type</typeparam>
+        /// <param name="T">specified type to create</param>
+        /// <returns>delegate</returns>
+        public static Func<T2> MakeNewDelegate<T2>(Type T)
+        {
+            MethodInfo methodInfo = typeof(InterfaceWrapper).GetMethod(
+                    "CreateNewObject",
+                    BindingFlags.NonPublic | BindingFlags.Static
+                );
+
+            MethodInfo genericMethod = methodInfo.MakeGenericMethod(
+                    (new Type[] { T, typeof(T2) })
+                );
+
+            return Delegate.CreateDelegate(typeof(Func<T2>), genericMethod) as Func<T2>;
+        }
+
+        private static Action<knownT, knownT2> Purchase1<T, T2, knownT, knownT2>(MethodInfo method)
+            where T : knownT
+            where T2 : knownT2
+        {
+            // create first delegate. It is not fine because its 
+            // signature contains unknown types T and T2
+            Action<T, T2> d = (Action<T, T2>)Delegate.CreateDelegate(
+                    typeof(Action<T, T2>),
+                    method
+            );
+
+            // create another delegate having necessary signature. 
+            // It encapsulates first delegate with a closure
+            return delegate (knownT target, knownT2 p) { d((T)target, (T2)p); };
+        }
+
+        private static Func<knownT, knownT2, knownR> Walk1<T, T2, knownT, knownT2, knownR>(MethodInfo method)
+        where T : knownT
+        where T2 : knownT2
+        {
+            Func<T, T2, knownR> d = (Func<T, T2, knownR>)Delegate.CreateDelegate(typeof(Func<T, T2, knownR>), method);
+
+            return delegate (knownT target, knownT2 p) { return d((T)target, (T2)p); };
+        }
+
+        /// <summary>
+        /// Creates static caller delegate for specified method
+        /// </summary>
+        /// <typeparam name="T">signature of the delegate</typeparam>
+        /// <param name="method">method to surround</param>
+        /// <returns>caller delegate with specified signature</returns>
+        public static T MakeCallDelegate<T>(MethodInfo method)
+        {
+            // we're going to select necessary generic function 
+            // and parameterize it with specified signature
+
+            // 1. select function name accordingly to parameters count
+
+            string methodName = method.Name + method.GetParameters().Length.ToString();
+
+            // 2. create parametrization signature
+            List<Type> signature = new List<Type>();
+            // first type parameter is type of target object
+            signature.Add(method.DeclaringType);
+
+            //next parameters are real types of method arguments
+            foreach (ParameterInfo pi in method.GetParameters())
+            {
+                signature.Add(pi.ParameterType);
+            }
+
+            // last parameters are known types of method arguments
+            signature.AddRange(typeof(T).GetGenericArguments());
+
+            // 3. call generator function with Delegate.Invoke. 
+            // We can do it because the generator will be called only once. 
+            // Result will be cached somewhere then.
+            MethodInfo methodInfo = typeof(InterfaceWrapper).GetMethod(
+                    methodName,
+                    BindingFlags.NonPublic | BindingFlags.Static
+                );
+
+            MethodInfo genericMethod = methodInfo.MakeGenericMethod(signature.ToArray());
+
+            return (T)genericMethod.Invoke(null, new object[] { method });
+        }
+    }
+}
---
+++ FastLateBoundGenerics/Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.ComponentModel.Composition;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Reflection;
+using Plugin;
+using System.ComponentModel.Composition.Hosting;
+
+namespace FastLateBoundGenerics
+{
+
+    class Program
+    {
+        [Import]
+        Lazy<IPerson> Person;
+
+        private readonly CompositionContainer _container;
+
+        public readonly IPerson mefPerson = this.Person.Value;
+
+        private Program()
+        {
+            AggregateCatalog catalog = new AggregateCatalog();
+            DirectoryCatalog dirCatalog = new DirectoryCatalog(Environment.CurrentDirectory);
+            catalog.Catalogs.Add(dirCatalog);
+            _container = new CompositionContainer(catalog);
+
+            try
+            {
+                _container.ComposeParts(this);
+            }
+            catch (CompositionException compositionException)
+            {
+                Console.WriteLine(compositionException.ToString());
+                Console.ReadLine();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            Program program = new Program();
+
+            ////find unknown types and method by names
+            //Type unknownPerson = Type.GetType("Plugin.Rob");
+            //Type unknownDog = Type.GetType("Plugin.Shadow");
+            //MethodInfo walkMethod = unknownPerson.GetMethod("Walk");
+            //MethodInfo purchaseMethod = unknownPerson.GetMethod("Purchase");
+
+            ////create and store delegates
+            //Func<IDog> NewDogDelegate = InterfaceWrapper.MakeNewDelegate<IDog>(unknownDog);
+            //Func<IPerson> NewPersonDelegate = InterfaceWrapper.MakeNewDelegate<IPerson>(unknownPerson);
+            //Func<IPerson, IDog, IDog> walkDelegate = InterfaceWrapper.MakeCallDelegate<Func<IPerson, IDog, IDog>>(walkMethod);
+            //Action<IPerson, IDog> purchaseDelegate = InterfaceWrapper.MakeCallDelegate<Action<IPerson, IDog>>(purchaseMethod);
+
+
+            ////call delegates
+            //IPerson personDelegate = NewPersonDelegate();
+            //IDog dogDelegate = NewDogDelegate();
+            //purchaseDelegate(personDelegate, dogDelegate);
+
+            //benchmark
+            IDog result;
+            int times = 50000000;
+
+            #region direct call 
+            int i = times;
+            IPerson person = new Plugin.Rob();
+            IDog dog = new Plugin.Shadow();
+            DateTime start = DateTime.Now;
+
+            while (i-- > 0)
+                result = person.Walk(dog);
+
+            Console.WriteLine("Direct call - {0}", (DateTime.Now - start).TotalMilliseconds);
+            #endregion
+
+            #region my way
+            //i = times;
+            //start = DateTime.Now;
+
+            //while (i-- > 0)
+            //    result = walkDelegate(personDelegate, dogDelegate);
+
+            //Console.WriteLine("GenericDelegates - {0}", (DateTime.Now - start).TotalMilliseconds);
+            #endregion
+
+            #region Invoke
+            //i = times;
+            //object[] paramArray = new object[] { dog };
+            //start = DateTime.Now;
+
+            //while (i-- > 0)
+            //    result = (IDog)walkMethod.Invoke(person, paramArray);
+
+            //Console.WriteLine("Invoke - {0}", (DateTime.Now - start).TotalMilliseconds);
+            #endregion
+
+            #region MEF
+            i = times;
+            
+
+            start = DateTime.Now;
+
+            while (i-- > 0)
+                result = program.Person.Value.Walk(dog);
+
+            Console.WriteLine("MEF - {0}", (DateTime.Now - start).TotalMilliseconds);
+            #endregion
+
+            Console.ReadKey();
+        }
+    }
+
+}
